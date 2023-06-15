684a619f04f15b278eb4f4cb00d817b30ab62466
Murilo Azevedo Jacon <70816232+muriloAJacon@users.noreply.github.com>
2023-06-14 21:26:01 -0300

    Started API (#2)
    * Started API
    
    * Added database entities

---
+++ API/Context/MySQLContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Context;
+
+public partial class MySQLContext : DbContext
+{
+    public MySQLContext(DbContextOptions<MySQLContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Recording> Recordings { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Recording>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("pk_Recording");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+
+            entity.HasOne(d => d.User).WithMany(p => p.Recordings)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_Recording_User");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("pk_User");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ API/Entities/Recording.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities;
+
+[Table("Recording", Schema = "dbo")]
+public partial class Recording
+{
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Column("user_id")]
+    public Guid UserId { get; set; }
+
+    [Column("created_at", TypeName = "timestamp(3) without time zone")]
+    public DateTime CreatedAt { get; set; }
+
+    [Column("finished_at", TypeName = "timestamp(3) without time zone")]
+    public DateTime? FinishedAt { get; set; }
+
+    [ForeignKey("UserId")]
+    [InverseProperty("Recordings")]
+    public virtual User User { get; set; } = null!;
+}
