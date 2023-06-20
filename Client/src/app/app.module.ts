06513c3c72988651ce1e3d5110154c3eff03837b
Murilo Azevedo Jacon <murilo.ajac@gmail.com>
2023-06-19 21:08:48 -0300

    Improvements to client

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -6,30 +6,35 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCheck, faComputer, faServer, faVideo } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faCircleStop, faCloudArrowUp, faComputer, faHouseLaptop, faServer, faVideo } from '@fortawesome/free-solid-svg-icons';
 import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FontAwesomeModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		FontAwesomeModule,
+		NgbModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule {
 	constructor(library: FaIconLibrary) {
 		library.addIcons(
 			faVideo,
 			faComputer,
-			faServer
+			faServer,
+			faCloudArrowUp,
+			faHouseLaptop,
+			faCircleStop,
 		)
 	}
 }
--- Client/src/app/home/home.component.ts
+++ Client/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DateTime } from 'luxon';
+import * as signalR from '@microsoft/signalr';
 
 @Component({
 	selector: 'app-home',
@@ -7,26 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-	private mediaRecorder: MediaRecorder | undefined;
+	public mediaRecorder: MediaRecorder | undefined;
 	private recordedChunks: BlobPart[] = [];
+
+	public startedAt: DateTime | undefined;
+
+	private hubConnection: signalR.HubConnection | undefined;
+
+	public get recordingDuration(): string {
+		if (!this.startedAt) {
+			return '00:00:00';
+		}
+
+		const duration = DateTime.now().diff(this.startedAt);
+		return duration.toFormat('hh:mm:ss');
+	}
 	
 	constructor() { }
 
 	ngOnInit(): void {
+		setInterval(() => {
+			this.startedAt = this.startedAt;
+		}, 1000);
 	}
 
 	startRecording() {
 		navigator.mediaDevices.getDisplayMedia({ video: true })
 			.then((stream: MediaStream) => {
 				this.recordedChunks = [];
+
 				this.mediaRecorder = new MediaRecorder(stream, { mimeType: 'video/webm; codecs=vp9' });
 				this.mediaRecorder.ondataavailable = (event) => {
 					if (event.data && event.data.size > 0) {
 						console.log(`pushing data`)
 						this.recordedChunks.push(event.data);
+
+						this.saveRecording();
 					}
 				};
 				this.mediaRecorder.start();
+				this.startedAt = DateTime.now();
 			})
 			.catch((error) => {
 				console.error('Error accessing media devices.', error);
@@ -41,5 +63,13 @@ export class HomeComponent implements OnInit {
 		a.download = 'screen-recording.webm';
 		document.body.appendChild(a);
 		a.click();
+		window.URL.revokeObjectURL(url);
+	}
+
+	stopRecording() {
+		this.mediaRecorder?.stop();
+		this.mediaRecorder?.stream.getTracks().forEach(track => track.stop());
+		
+		this.startedAt = undefined;
 	}
 }
